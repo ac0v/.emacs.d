7de60e97f4a65d6a7be36ea9631d2928cfdad5e5
spebern <bernhard@specht.net>
2017-05-29 15:34:12 +0200

    nicer org latex export

--- init.el
+++ init.el
@@ -708,6 +708,18 @@
   (setq org-capture-templates
 	'(("t" "Todo" entry (file "~/Dropbox/org/index.org" "")
 	   "* TODO %?\n"))))
+;; Include the latex-exporter
+(require 'ox-latex)
+;; Add minted to the defaults packages to include when exporting.
+(add-to-list 'org-latex-packages-alist '("" "minted"))
+;; Tell the latex export to use the minted package for source
+;; code coloration.
+(setq org-latex-listings 'minted)
+;; Let the exporter use the -shell-escape option to let latex
+;; execute external programs.
+;; This obviously and can be dangerous to activate!
+(setq org-latex-pdf-process
+      '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 
 
 ;; select date when todo changed state
