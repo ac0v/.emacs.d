6c2855d25c31e75945036d7322c1b45528f7d976
spebern <bernhard@specht.net>
2017-07-27 14:28:20 +0200

    remove slack

--- init.el
+++ init.el
@@ -1085,44 +1085,6 @@
       "/usr/share/languagetool:/usr/share/java/languagetool/*")
   )
 
-(use-package slack
-  :commands (slack-start)
-  :init
-  (setq slack-buffer-emojify t) ;; if you want to enable emoji, default nil
-  (setq slack-prefer-current-team t)
-  :config
-  (slack-register-team
-   :name "nsttalk"
-   :default t
-   :client-id "205145334934.204256453843"
-   :client-secret "a8e2749a46604d68d9bba07346e0186b"
-   :token "oLZ8iIHr7MWuIotxYlzMPqAL"
-   :subscribed-channels '(test-rename rrrrr))
-
-  (evil-define-key 'normal slack-info-mode-map
-    ",u" 'slack-room-update-messages)
-  (evil-define-key 'normal slack-mode-map
-    ",c" 'slack-buffer-kill
-    ",ra" 'slack-message-add-reaction
-    ",rr" 'slack-message-remove-reaction
-    ",rs" 'slack-message-show-reaction-users
-    ",pl" 'slack-room-pins-list
-    ",pa" 'slack-message-pins-add
-    ",pr" 'slack-message-pins-remove
-    ",mm" 'slack-message-write-another-buffer
-    ",me" 'slack-message-edit
-    ",md" 'slack-message-delete
-    ",u" 'slack-room-update-messages
-    ",2" 'slack-message-embed-mention
-    ",3" 'slack-message-embed-channel
-    "\C-n" 'slack-buffer-goto-next-message
-    "\C-p" 'slack-buffer-goto-prev-message)
-   (evil-define-key 'normal slack-edit-message-mode-map
-    ",k" 'slack-message-cancel-edit
-    ",s" 'slack-message-send-from-buffer
-    ",2" 'slack-message-embed-mention
-    ",3" 'slack-message-embed-channel))
-
 (use-package alert
   :commands (alert)
   :init
